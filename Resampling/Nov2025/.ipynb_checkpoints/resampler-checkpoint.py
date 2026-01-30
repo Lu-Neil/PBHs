7e635e57d9d839bd9c6e50be5356f6fc5e9c9b55
Neil Lu <neil.lu@anu.edu.au>
2026-01-30 18:44:33 +1100

    Changed resampler, PBH+5vec works

--- Resampling/Nov2025/.ipynb_checkpoints/resampler-checkpoint.py
+++ Resampling/Nov2025/.ipynb_checkpoints/resampler-checkpoint.py
@@ -24,14 +24,15 @@ class Resampler(object):
     def nufft(self) -> None:
         signal = self.timeseries
         tau = self.resampled_time
-        bin_no = len(tau) # make it an input
-        
-        scale = abs((2*np.pi) / (tau[-1] - tau[0]))
-        tau_scaled = scale * (tau-tau[0]) - np.pi
+        bin_no = len(tau)
+
+        # Rescale time to [-pi, pi)
+        scale = (2*np.pi) / (tau[-1] - tau[0]) 
+        tau_scaled = scale * (tau-tau[0])
         bins = np.arange(bin_no) - bin_no//2
         freqs = bins * scale
         
-        weights = finufft.nufft1d1(tau_scaled, signal.astype(complex), bin_no)
+        weights = finufft.nufft1d1(tau_scaled, signal.astype(complex), bin_no, isign = -1)
         self.freqs = freqs
         self.weights = weights
 
@@ -41,15 +42,15 @@ class Resampler(object):
         assert np.isrealobj(signal), "Timeseries is not real"
 
         scale = abs((2*np.pi) / (tau[-1] - tau[0]))
-        tau_scaled = scale * (tau-tau[0]) - np.pi
+        tau_scaled = scale * abs(tau-tau[0]) - np.pi
         
-        Nf = len(tau)//2
-        N0 = Nf // 2
-        bins = np.arange(Nf) # no negative frequencies
+        bin_no = len(tau)//2
+        normalisation = bin_no // 2
+        bins = np.arange(bin_no) # no negative frequencies
         freqs = bins * scale
-        signal_shifted = signal * np.exp(1j * N0 * tau_scaled)
+        signal_shifted = signal * np.exp(1j * normalisation * tau_scaled)
 
-        weights = finufft.nufft1d1(tau_scaled, signal_shifted, Nf)
+        weights = finufft.nufft1d1(tau_scaled, signal_shifted, bin_no, isign = 1)
         self.freqs = freqs
         self.weights = weights
 
--- Resampling/Nov2025/PBH-5vec.py
+++ Resampling/Nov2025/PBH-5vec.py
@@ -63,7 +63,7 @@ Mc = 3e-2 * 2e30
 f_max = 4
 beta = const*f0**(8/3)*Mc**(5/3)
 f_signal = 4*f_max
-nt = round(f_signal*T_obs)
+nt = round(f_signal*T_obs)+1
 
 ref_time = Time('2019-04-03')
 t = ref_time.gps + np.arange(nt)/f_signal
@@ -71,7 +71,7 @@ t = Time(t, format="gps", scale="utc")
 t_offset = t.gps-t.gps[0]
 
 phi = -6*pi/5*f0*(1-8./3.*(beta)*t_offset)**(5/8)/beta
-signal_source = 1*np.exp(-1j*phi)
+signal_source = 1*np.exp(1j*phi)
 
 # %%
 resampler = Resampler()
@@ -123,79 +123,27 @@ sidereal.compute_H()
 sidereal.compute_A(gmst(t.mjd))
 sidereal.compute_5vec()
 amp_modulation = h0 * sidereal.amp_modulation
-signal_det = sidereal.amp_modulation * signal_source
+signal_det = amp_modulation * signal_source
 
 # %%
-phi = gmst(t.mjd[0]) + params['ra'] - params['lng']
+sidereal_phase = gmst(t.mjd[0]) + params['ra'] - params['lng']
 A_recovery = sidereal.A.copy()
 Ap_recovery = sidereal.A_p.copy()
 Ac_recovery = sidereal.A_c.copy()
 
 for i in range(5):
-    A_recovery[i] = sidereal.A[i] * np.exp(1j*(i-2)*phi)
-    Ap_recovery[i] = sidereal.A_p[i] * np.exp(1j*(i-2)*phi)
-    Ac_recovery[i] = sidereal.A_c[i] * np.exp(1j*(i-2)*phi)
+    A_recovery[i] = sidereal.A[i] * np.exp(1j*(i-2)*sidereal_phase)
+    Ap_recovery[i] = sidereal.A_p[i] * np.exp(1j*(i-2)*sidereal_phase)
+    Ac_recovery[i] = sidereal.A_c[i] * np.exp(1j*(i-2)*sidereal_phase)
 
 # %%
 resampler = Resampler()
 resampler.timeseries = amp_modulation
-resampler.resampled_time = -t.gps
+resampler.resampled_time = t.gps
 resampler.nufft()
 pl.plot(resampler.freqs/(2*np.pi), resampler.power_normalized, 'o')
-pl.plot(fft_freqs, abs(fft_amps)**2, 'o')
+# pl.plot(fft_freqs, abs(fft_amps)**2, 'o')
 pl.xlim(- 10/side_day, 10/side_day)
-
-# %%
-signal = amp_modulation
-tau = t.gps
-bin_no = len(tau) # make it an input
-
-scale = abs((2*np.pi) / (tau[-1] - tau[0]))
-tau_scaled = scale * (tau-tau[0]) - np.pi
-bins = np.arange(bin_no) - bin_no//2
-freqs = bins * scale
-
-weights = finufft.nufft1d1(tau_scaled, signal.astype(complex), bin_no, isign=1)
-test_freqs0 = freqs
-test_weights0 = weights
-
-# %%
-scale
-
-# %%
-tau_scaled
-
-# %%
-signal = amp_modulation
-tau = -t.gps
-bin_no = len(tau) # make it an input
-
-scale = abs((2*np.pi) / (tau[-1] - tau[0]))
-tau_scaled = scale * abs((tau-tau[0])) - np.pi
-bins = np.arange(bin_no) - bin_no//2
-freqs = bins * scale
-
-weights = finufft.nufft1d1(-tau_scaled, signal.astype(complex), bin_no)
-test_freqs1 = freqs
-test_weights1 = weights
-
-# %%
-scale
-
-# %%
-tau_scaled
-
-# %%
-pl.plot(test_freqs0, abs(test_weights0)**2)
-pl.plot(test_freqs1, abs(test_weights1)**2)
-pl.plot(fft_freqs*2*np.pi, abs(fft_amps*len(amp_modulation))**2)
-pl.xlim(-50/side_day, 50/side_day)
-
-# %%
-test_weights0
-
-# %%
-test_weights1
 
 # %%
 nufft_freqs = resampler.freqs/(2*np.pi) #np.fft.fftshift(np.fft.fftfreq(len(amp_modulation), d=np.diff(t.gps)[0]))
@@ -224,38 +172,12 @@ print(f"FFT h_est={fft_h_est}")
 print(f"True h0={h0}")
 
 # %%
-fft_freqs[abs(fft_freqs-(-1/side_day)).argmin()]
-
-# %%
-
-
-X = np.empty((5), dtype=complex)
-
-X[0] = fft_amps[abs(fft_freqs-(-2/side_day)).argmin()]
-X[1] = fft_amps[abs(fft_freqs-(-1/side_day)).argmin()]
-X[2] = fft_amps[abs(fft_freqs).argmin()]
-X[3] = fft_amps[abs(fft_freqs-(+1/side_day)).argmin()]
-X[4] = fft_amps[abs(fft_freqs-(+2/side_day)).argmin()]
-
-h_est = np.dot(X, np.conj(A_recovery))/np.sum(np.abs(A_recovery)**2)
-hp_est = np.dot(X, np.conj(Ap_recovery))/np.sum(np.abs(Ap_recovery)**2)
-hc_est = np.dot(X, np.conj(Ac_recovery))/np.sum(np.abs(Ac_recovery)**2)
-print(h_est)
-print(h0)
-
-# %%
-fft_freqs[abs(fft_freqs-(-1/side_day)).argmin()]
-
-# %%
 resampler = Resampler()
 resampler.timeseries = signal_det
 resampler.resampled_time = t_offset
 resampler.nufft()
 pl.plot(resampler.freqs/(2*np.pi), resampler.power_normalized, 'o')
 pl.xlim(f0 - 10/side_day, f0 + 10/side_day)
-
-# %%
-A_recovery
 
 # %%
 X = np.empty((5), dtype=complex)
@@ -267,299 +189,6 @@ X[3] = resampler.weights_normalized[abs(resampler.freqs/(2*np.pi)-(f0+1/side_day
 X[4] = resampler.weights_normalized[abs(resampler.freqs/(2*np.pi)-(f0+2/side_day)).argmin()]
 
 print(abs(np.dot(X, np.conj(A_recovery))/np.sum(np.abs(A_recovery)**2)))
-
-# %%
-h0
-
-# %%
-f = f_calc(f0, beta, t)
-d = 8e3*3.086e+16 # 8kpc
-h0 = 4/d*(G*Mc/c**2)**(5/3)*(pi*f/c)**(2/3)
-
-# %%
-params = dict(ra = 0.5, dec = 0.5, eta = 0.5, psi = 0.5, lat = 0.5, 
-              lng = 0.5, az = 0.5, side_day=side_day)
-sidereal = five_vec(**params)
-sidereal.compute_H()
-sidereal.compute_A(t)
-sidereal.compute_5vec()
-amp_modulation = sidereal.amp_modulation
-
-# %%
-signal = amp_modulation*freq_signal # * h0
-
-fft_freqs, fft_amps = nufft(beta, signal, t, len(signal))
-f0_idx = abs(fft_freqs-f0).argmin()
-
-# %%
-import matplotlib as mpl
-mpl.rcParams['figure.dpi'] = 300
-
-# %%
-pl.plot(fft_freqs, abs(fft_amps)**2, 'o')
-pl.xlim(f0-0.005, f0+0.005)
-pl.axvline(fft_freqs[f0_idx-2*n_days], c='r', ls='--')
-pl.axvline(fft_freqs[f0_idx-1*n_days], c='r', ls='--')
-pl.axvline(fft_freqs[f0_idx], c='r', ls='--')
-pl.axvline(fft_freqs[f0_idx+1*n_days], c='r', ls='--')
-pl.axvline(fft_freqs[f0_idx+2*n_days], c='r', ls='--')
-pl.xlabel("Frequency")
-pl.ylabel("Power")
-pl.title("Demonstration of the 5-vector statistic")
-pl.tight_layout()
-
-# %%
-X = np.empty((5), dtype=complex)
-
-f0_idx = abs(fft_freqs-f0).argmin()
-X[0] = fft_amps[f0_idx-6]
-X[1] = fft_amps[f0_idx-3]
-X[2] = fft_amps[f0_idx]
-X[3] = fft_amps[f0_idx+3]
-X[4] = fft_amps[f0_idx+6]
-
-# %%
-estimator = abs(np.dot(X, np.conj(sidereal.A))/np.sum(np.abs(sidereal.A)**2))
-estimator
-
-# %%
-estimator
-
-# %%
-# pl.plot(t[::100], h0[::100])
-pl.axhline(estimator, c='r', ls='--', label = 'Estimator')
-pl.axhline(2.4306/pi, c='tab:orange', label="Theoretical avg FFT loss")
-pl.legend()
-pl.xlabel("Time")
-pl.ylabel("h0")
-
-# %%
-2.4306/pi
-
-# %% [markdown]
-# ## Coverage test
-
-# %%
-c = 3e8
-G = 6.67e-11
-pi = np.pi
-const = 96/5*pi**(8/3)*(G/c**3)**(5/3)
-
-f_max = 200
-side_day = 1e4/3 #86164.09053083288
-n_days = 3
-T_obs = n_days*side_day
-f_signal = 3*f_max
-nt = round(f_signal*T_obs)
-t = (np.arange(nt)/f_signal).astype(np.longdouble)
-d = 8e3*3.086e+16 # 8kpc
-
-params = dict(ra = 0.5, dec = 0.5, eta = 0.5, psi = 0.5, lat = 0.5, 
-              lng = 0.5, az = 0.5, side_day=side_day)
-sidereal = five_vec(**params)
-sidereal.compute_H()
-sidereal.compute_A(t)
-sidereal.compute_5vec()
-amp_modulation = sidereal.amp_modulation
-
-# %%
-nufft_results = []
-fivevec_results=[]
-for i in range(5):
-    print(i)
-    f0 = np.linspace(20, 40, 10, endpoint=False)[i]
-    Mc = 3e-2 * 2e30
-    beta = const*f0**(8/3)*Mc**(5/3)
-
-    phi = -6*pi/5*f0*(1-8./3.*(beta)*t)**(5/8)/beta
-    freq_signal = 1*np.exp(1j*phi)
-    temp_freqs, temp_amps = nufft(beta, freq_signal, t, len(freq_signal))
-    nufft_results.append(max(abs(temp_amps)**2))
-    
-    f = f_calc(f0, beta, t)
-    h0 = 4/d*(G*Mc/c**2)**(5/3)*(pi*f/c)**(2/3)
-    signal = h0[0]*amp_modulation*freq_signal
-
-    fft_freqs, fft_amps = nufft(beta, signal, t, len(signal))
-    f0_idx = abs(fft_freqs-f0).argmin()
-    X = np.empty((5), dtype=complex)
-    X[0] = fft_amps[f0_idx-n_days*2]
-    X[1] = fft_amps[f0_idx-n_days*1]
-    X[2] = fft_amps[f0_idx]
-    X[3] = fft_amps[f0_idx+n_days*1]
-    X[4] = fft_amps[f0_idx+n_days*2]
-    
-    estimator = abs(np.dot(X, np.conj(sidereal.A))/np.sum(np.abs(sidereal.A)**2))
-    fivevec_results.append(estimator/h0[0])
-
-# %%
-pl.hist(nufft_results)
-pl.axvline(2.4306/pi, c='r', ls='--')
-
-# %%
-pl.hist(np.multiply(2, fivevec_results))
-pl.axvline(2.4306/pi, c='r', ls='--')
-
-# %%
-pl.hist(np.divide(fivevec_results,nufft_results))
-
-# %% [markdown]
-# ## Additional loss from amp-modulation
-
-# %%
-f0 = 20
-Mc = 4e-2 * 2e30
-beta = const*f0**(8/3)*Mc**(5/3)
-
-f_max = 200
-side_day = 1e4/3 #86164.09053083288
-n_days = 3
-T_obs = n_days*side_day
-f_signal = 3*f_max
-nt = round(f_signal*T_obs)
-t = (np.arange(nt)/f_signal).astype(np.longdouble)
-d = 8e3*3.086e+16 # 8kpc
-
-# Amp modulation
-params = dict(ra = 0.5, dec = 0.5, eta = 0.5, psi = 0.5, lat = 0.5, 
-              lng = 0.5, az = 0.5, side_day=side_day)
-sidereal = five_vec(**params)
-sidereal.compute_H()
-sidereal.compute_A(t)
-sidereal.compute_5vec()
-amp_modulation = sidereal.amp_modulation
-
-# %%
-phi = -6*pi/5*f0*(1-8./3.*(beta)*t)**(5/8)/beta
-freq_signal = 1*np.exp(1j*phi)
-
-#NUFFT
-bin_no = len(freq_signal)
-data=freq_signal
-bins = np.arange(bin_no) - bin_no//2
-
-tau = -3/5*(1-8/3*beta*t)**(5/8)/beta
-scale = (2*pi)/(tau[-1]-tau[0])
-tau *= scale
-start_diff = -pi-tau[0]
-tau += start_diff
-temp_amp = finufft.nufft1d1(
-    tau.astype(np.float64), data.astype(complex), bin_no, eps=1e-10, upsampfac=2.0)/len(data)
-temp_freq = -bins*scale/(2*pi)
-
-# amp modulation
-f = f_calc(f0, beta, t)
-h0 = 4/d*(G*Mc/c**2)**(5/3)*(pi*f/c)**(2/3)
-signal = h0[0]*amp_modulation*freq_signal
-
-# 5-vector
-fft_freqs, fft_amps = nufft(beta, signal, t, len(signal))
-f0_idx = abs(fft_freqs-f0).argmin()
-X = np.empty((5), dtype=complex)
-X[0] = fft_amps[f0_idx-n_days*2]
-X[1] = fft_amps[f0_idx-n_days*1]
-X[2] = fft_amps[f0_idx]
-X[3] = fft_amps[f0_idx+n_days*1]
-X[4] = fft_amps[f0_idx+n_days*2]
-
-estimator = abs(np.dot(X, np.conj(sidereal.A))/np.sum(np.abs(sidereal.A)**2))
-
-mono = 1*np.exp(1j*(2*pi*f0*t+phi[0]))
-modulated_mono = h0[0]*amp_modulation*mono
-
-# %%
-inverse = finufft.nufft1d2(tau.astype(np.float64), fft_amps, eps=1e-12, upsampfac=2.0)
-
-# %%
-# test = np.fft.ifft(fft_amps*len(signal))
-
-# %%
-pl.plot(t, np.abs(inverse)-np.abs(signal))
-pl.xlim(t[100], t[-100])
-pl.ylim(-0.25e-22, 0.25e-22)
-
-
-# %%
-def strobo(beta, data, f_ratio):
-    new_t = -3/5*(1-8/3*beta*t)**(5/8)/beta
-    f_new = f_signal/f_ratio
-    
-    new_t *= f_new #changes the frequency to the new downsampled frequency (approximately, not exactly)
-    floor_t = np.floor(new_t) #For some reason floor works better than round. Take it to the nearest time index which are integers
-    idx = np.nonzero(np.diff(floor_t)) #The step that downsamples
-    resampled = data[idx]
-    t_out = (new_t[idx])/f_new
-    return (resampled, t_out)
-
-
-# %%
-f0 = 20
-Mc = 4e-2 * 2e30
-beta = const*f0**(8/3)*Mc**(5/3)
-
-f_max = 200
-n_days = 3
-side_day = 1e4/n_days #86164.09053083288
-T_obs = n_days*side_day
-f_signal = 20*f_max
-nt = round(f_signal*T_obs)
-t = (np.arange(nt)/f_signal).astype(np.longdouble)
-d = 8e3*3.086e+16 # 8kpc
-
-# Amp modulation
-params = dict(ra = 0.5, dec = 0.5, eta = 0.5, psi = 0.5, lat = 0.5, 
-              lng = 0.5, az = 0.5, side_day=side_day)
-sidereal = five_vec(**params)
-sidereal.compute_H()
-sidereal.compute_A(t)
-sidereal.compute_5vec()
-amp_modulation = sidereal.amp_modulation
-
-# %%
-phi = -6*pi/5*f0*(1-8./3.*(beta)*t)**(5/8)/beta
-freq_signal = 1*np.exp(1j*phi)
-# strobo_data, strobo_time = strobo(beta, freq_signal, f_ratio = 8)
-# strobo_amp = np.fft.fftshift(np.fft.fft(strobo_data))
-# strobo_freq = np.fft.fftshift(np.fft.fftfreq(len(strobo_data), np.diff(strobo_time)[0]))
-
-# amp modulation
-f = f_calc(f0, beta, t)
-h0 = 4/d*(G*Mc/c**2)**(5/3)*(pi*f/c)**(2/3)
-signal = h0[0]*amp_modulation*freq_signal
-
-# # 5-vector
-strobo_data, strobo_time = strobo(beta, signal, f_ratio = 8)
-strobo_amp = np.fft.fftshift(np.fft.fft(strobo_data))/len(strobo_data)
-strobo_freq = np.fft.fftshift(np.fft.fftfreq(len(strobo_data), np.diff(strobo_time)[0]))
-
-f0_idx = abs(strobo_freq-f0).argmin()
-X = np.empty((5), dtype=complex)
-X[0] = strobo_amp[f0_idx-n_days*2]
-X[1] = strobo_amp[f0_idx-n_days*1]
-X[2] = strobo_amp[f0_idx]
-X[3] = strobo_amp[f0_idx+n_days*1]
-X[4] = strobo_amp[f0_idx+n_days*2]
-
-estimator = abs(np.dot(X, np.conj(sidereal.A))/np.sum(np.abs(sidereal.A)**2))
-
-# mono = 1*np.exp(1j*(2*pi*f0*t+phi[0]))
-# modulated_mono = h0[0]*amp_modulation*mono
-
-# %%
-# pl.plot(strobo_freq, abs(strobo_amp/len(strobo_data))**2, 'o')
-# pl.axhline(2.4306/pi, c='r', ls='--')
-# pl.xlim(f0-0.003, f0+0.003)
-
-# %%
-pl.plot(strobo_freq, abs(strobo_amp)**2, 'o')
-pl.xlim(f0-0.005, f0+0.005)
-pl.axvline(strobo_freq[f0_idx-2*n_days], c='r', ls='--')
-pl.axvline(strobo_freq[f0_idx-1*n_days], c='r', ls='--')
-pl.axvline(strobo_freq[f0_idx], c='r', ls='--')
-pl.axvline(strobo_freq[f0_idx+1*n_days], c='r', ls='--')
-pl.axvline(strobo_freq[f0_idx+2*n_days], c='r', ls='--')
-
-# %%
-estimator/h0[0]
+print(h0)
 
 # %%
